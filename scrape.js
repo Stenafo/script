618139864b5b94855b5eab4fe54804f4e9f40ea8
Stefano <15031098+Stenafo@users.noreply.github.com>
2023-01-15 18:10:16 +0100

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,46 +1,49 @@
-const fs = require('fs');
-const Papa = require('papaparse');
-const cheerio = require('cheerio');
-
-//object containing valid roles for each element
+const fs = require("fs");
+const Papa = require("papaparse");
 const validRoles = {
-  picture: [],
-  div: ['region','alert','alertdialog','application','article','banner','button','checkbox','columnheader','combobox','complementary','contentinfo','definition','dialog','directory','document','feed','figure','form','grid','gridcell','group','heading','img','link','list','listbox','listitem','log','main','marquee','math','menu','menubar','menuitem','menuitemcheckbox','menuitemradio','navigation','note','option','presentation','progressbar','radio','radiogroup','range','region','roletype','row','rowgroup','rowheader','scrollbar','search','searchbox','separator','slider','spinbutton','status','tab','table','tablist','tabpanel','textbox','timer','toolbar','tooltip','tree','treegrid','treeitem'],
-  body: ['document','application'],
-  header: ['banner','complementary','contentinfo','form','main','navigation','search'],
-  article: ['article','document','application','main'],
-  nav: ['navigation','directory','list','listitem','listbox','menu','menubar','menuitem','menuitemcheckbox','menuitemradio','tablist','tabpanel','toolbar'],
-  aside: ['complementary','note','search','status']
+    "picture": [],
+    "div": ["alert","alertdialog","application","document","log","marquee","status","timer"],
+    "body": ["document"],
+    "a": ["button","link"],
+    "button": ["button"],
+    "header": ["banner","complementary","contentinfo","form","main","navigation","search"]
 };
 
-function checkSnippet(snippet) {
-  //create a new DOM element using cheerio
-  const $ = cheerio.load(snippet);
-  const element = $('*')[0];
-  const elementName = element.name;
+fs.readFile("test.csv", "utf-8", (err, data) => {
+    if (err) throw err;
 
-  if (!validRoles[elementName]) {
-    return "This element does not have a role";
-  }
-  const role = element.attribs.role;
-  if (!validRoles[elementName].includes(role)) {
-    return "This element does not have a valid role";
-  }
-  return "";
-}
-
-//parse the csv file
-Papa.parse("test.csv", {
-  header: true,
-  complete: function(results) {
-    const data = results.data;
-    //iterate over rows of the csv
-    data.forEach((row) => {
-      const howToFix = checkSnippet(row["Snippet"]);
-      row["How to fix"] = howToFix;
+    Papa.parse(data, {
+        header: true,
+        complete: results => {
+            var output = results.data.map(row => {
+                var element = row.snippet.match(/<([a-z]+)[^>]*>/)[1];
+                var error = "";
+                if (validRoles[element]) {
+                    var role = row.snippet.match(/role=["']([^"']+)["']/);
+                    if (role) {
+                        role = role[1];
+                        if (validRoles[element].indexOf(role) === -1) {
+                            error = `Invalid role ${role} for element ${element}`;
+                        }
+                    }
+                }
+                return {
+                    path: row.path,
+                    snippet: row.snippet,
+                    howToFix: error
+                }
+            });
+            Papa.unparse(output, {
+                header: true,
+                quotes: true,
+                quoteChar: '"',
+                delimiter: ",",
+                newline: "\r\n",
+                escapeChar: "\\",
+                encoding: "utf-8",
+                download: true,
+                filename: "test.output.csv"
+            });
+        }
     });
-    //stringify the modified data and write to a new csv file
-    const csvData = Papa.unparse(data);
-    fs.writeFileSync("test-output.csv", csvData);
-  }
 });
